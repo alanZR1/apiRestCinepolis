776d2c89c6828c704eeabe548f10f3513cee5fe9
Alan Reyes Luis <alannreyes02@gmail.com>
2026-04-17 22:19:28 -0600

    corrigiendo peticiones hhtp de webservice

--- proyectoCinepolis/src/main/java/com/mx/proyectoCinepolis/controller/PeliculasWebService.java
+++ proyectoCinepolis/src/main/java/com/mx/proyectoCinepolis/controller/PeliculasWebService.java
@@ -24,17 +24,18 @@ import com.mx.proyectoCinepolis.service.PeliculasServImp;
 
 public class PeliculasWebService {
 
-    //@RestController: controlador de solicitudes REST
+    //@RestController: controlador de solicitudes REST recibe request http y devuelve json
     //@RequestMapping: ruta base del servicio
     //@CrossOrigin: permite peticiones desde otros orígenes (evita bloqueos del navegador)
 
-    @Autowired
+    @Autowired // inyección de dependencias para usar los métodos del servicio
     PeliculasServImp peliculasServImp;
 
     // Peticiones: GET, POST, PUT...
     // URL de prueba en Postman:
     // http://localhost:9000/PeliculasWebService/mostrar
 
+	// los metodos la mejor forma es verlos como acciones http 
     @GetMapping(path = "mostrar")
     public List<Peliculas> mostrar() {
         return peliculasServImp.mostrar();
@@ -42,18 +43,18 @@ public class PeliculasWebService {
     
     // http://localhost:9000/PeliculasWebService/guardar
 	@PostMapping (path = "guardar")
-	public ResponseEntity<?> guardar(@RequestBody Peliculas pelicula){	
+	public ResponseEntity<?> guardar(@RequestBody Peliculas pelicula){  // @RequestBody: convierte el JSON del cuerpo de la solicitud en un objeto Peliculas	
 		String response = peliculasServImp.guardar(pelicula);
 		
 		if (response.equals("nombreYaExiste")) {
-			return new ResponseEntity<>("Ese nombre ya existe, no se puede guardar ", HttpStatus.OK);
+			return new ResponseEntity<>("Ese nombre ya existe, no se puede guardar ", HttpStatus.BAD_REQUEST);
 		}else {
-			return new ResponseEntity<>(pelicula, HttpStatus.CREATED);
+			return new ResponseEntity<>(pelicula, HttpStatus.CREATED); // devuelve el objeto guardado y un código de estado HTTP 201 (Created)
 		}
 	}
 
 	
-	@PostMapping(path = "buscar")
+	@GetMapping(path = "buscar")
 	public Peliculas buscarXid(@RequestBody Peliculas pelicula) {
 		return peliculasServImp.buscarXid(pelicula.getIdPelicula());
 	}
@@ -61,7 +62,7 @@ public class PeliculasWebService {
 	
 	@PutMapping( path = "editar")
 	public ResponseEntity<?> editar(@RequestBody Peliculas pelicula){
-		boolean response = peliculasServImp.editar(pelicula);
+		boolean response = peliculasServImp.editar(pelicula); // devuelve true si se editó correctamente, false si no se encontró el registro para editar
 		
 		if (response == false) {
 			return new ResponseEntity<>("Ese registro no existe, no se puede editar", HttpStatus.OK);
@@ -71,17 +72,16 @@ public class PeliculasWebService {
 	}
 	
 
-	@PostMapping(path = "eliminar")
+	@DeleteMapping(path = "eliminar")
 	public ResponseEntity<String> eliminar (@RequestBody Peliculas pelicula){
 		boolean response = peliculasServImp.eliminar(pelicula.getIdPelicula());
 		
 		if (response == true) {
 			return new ResponseEntity<>("Registro eliminado con exito", HttpStatus.OK);
 		}else {
-			return new ResponseEntity<>("Registro no eliminado, no existe", HttpStatus.OK);
+			return new ResponseEntity<>("Registro no eliminado, no existe", HttpStatus.NOT_FOUND);
 		}
-		}
-	
+	}
 	
 	// http://localhost:9000/PeliculasWebService/buscarXnombre/nombre
 	@GetMapping(path = "buscarXnombre/{nombre}")
